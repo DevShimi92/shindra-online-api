cdfe4fa046c700d5ad1b84dfce9957a6bf9e1d90
DevShimi92 <hospitalier.philippe@gmail.com>
2020-07-09 18:33:03 +0200

    Update jwtToken.js

--- jwtToken.js
+++ jwtToken.js
@@ -5,7 +5,7 @@ module.exports = jwtRoute;
 
 function jwtRoute() {
     console.log(process.env.NODE_ENV+"*");
-    if (process.env.NODE_ENV != 'test ' )
+    if (process.env.NODE_ENV == 'test ' )
             {
                 console.log('B');
                   const { secret } = { "secret" : 'MySecretIsNotHere' };
